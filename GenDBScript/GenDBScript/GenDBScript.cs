31f47d183ce59b4e9b4f553bdd08f75fa3c87677
Xabi Ochotorena <github@ochoto.com>
2013-06-03 17:25:54 +0200

    Primer Spike objeto Transfer

--- GenDBScript/GenDBScript/GenDBScript.cs
+++ GenDBScript/GenDBScript/GenDBScript.cs
@@ -12,6 +12,28 @@ namespace GenDBScript {
     class GenDBScript {
         static void Main(string[] args) {
 
+            Server s = new Server("CENTSQL18");
+            foreach (Database dd in s.Databases) {
+                System.Console.WriteLine("-- " + dd.Name);
+            }
+            System.Console.ReadLine();
+
+            Database d = s.Databases["REPL_SIB"];
+            //ScriptingOptions databaseOptions = new ScriptingOptions();
+            //databaseOptions.Add(ScriptOption.WithDependencies);
+            //StringCollection crtDb = d.Script(databaseOptions);
+
+            //Transfer t = new Transfer(d);
+            Transfer t = new Transfer(d);
+
+            StringCollection crtDb = t.ScriptTransfer();
+            foreach (String crtS in crtDb) {
+                System.Console.WriteLine(crtS);
+            }
+
+            System.Console.ReadLine();
+
+
         }
     }
 }
